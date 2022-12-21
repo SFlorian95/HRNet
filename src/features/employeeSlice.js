8665e57331af6f6f9b419d475d1801a01a6fdf6e
SFlorian95 <subramanyam.florian@outlook.fr>
2022-12-21 15:39:03 +0100

    Création du tableaux listant les employées et des features accompagnant la page listEmplyee

--- src/features/employeeSlice.js
+++ src/features/employeeSlice.js
@@ -91,20 +91,119 @@ const employees = [
   },
 ]
 
-const createEmployeeSlice = createSlice({
-  name: 'createEmployee',
+const initEntries = () => employees.filter((item, index) => index < 5)
+
+const computeEntries = (state) => {
+  const startIndex = (state.currentPagination - 1) * state.showEntries
+  const endIndex =
+    state.currentPagination * state.showEntries > state.employees.length
+      ? state.employees.length
+      : state.currentPagination * state.showEntries
+
+  const tempEmployees = state.searchValue
+    ? state.employees.filter((item) =>
+        atOnceKeyIncludesValue(state.searchValue, item)
+      )
+    : state.employees
+  state.filteredLength = tempEmployees.length
+  // compute pagination length
+  state.countPaginations =
+    Math.floor(tempEmployees.length / parseInt(state.showEntries)) + 1
+  state.entries = tempEmployees.filter(
+    (item, index) => index >= startIndex && index < endIndex
+  )
+  setShowing(state)
+}
+
+const atOnceKeyIncludesValue = (value, item) => {
+  const keys = Object.keys(item)
+  let result = false
+  for (const key of keys) {
+    if (item[key].toLowerCase().includes(value.toLowerCase())) {
+      result = true
+    }
+  }
+  return result
+}
+
+const setShowing = (state) => {
+  const start = (state.currentPagination - 1) * state.showEntries + 1
+  const paginationLength =
+    state.currentPagination * state.showEntries > state.filteredLength
+      ? state.filteredLength
+      : state.currentPagination * state.showEntries
+  const totalFiltered =
+    state.filteredLength < state.employees.length
+      ? `(filtered from ${state.employees.length} total entries)`
+      : ''
+  state.showing = `Showing ${start} to ${paginationLength} of ${state.filteredLength} entries ${totalFiltered}`
+}
+
+const employeeSlice = createSlice({
+  name: 'employee',
   initialState: {
     employees: employees,
+    sortedBy: { name: '', type: 'ASC' },
+    showEntries: 5,
+    countPaginations: Math.floor(employees.length / 5) + 1,
+    currentPagination: 1,
+    entries: initEntries(),
+    searchValue: '',
+    showing: `Showing 1 to 5 of ${employees.length} entries`,
+    filteredLength: employees.length,
   },
   reducers: {
     add: (state, action) => {
       state.employees.push(action.payload)
-    }
+      computeEntries(state)
+    },
+    sort: (state, action) => {
+      const asc = (a, b) => (a[action.payload] > b[action.payload] ? 1 : -1)
+      const desc = (a, b) => (a[action.payload] < b[action.payload] ? 1 : -1)
+      let sortType = asc
+      if (state.sortedBy.name === action.payload) {
+        // toggle type
+        sortType = state.sortedBy.type === 'ASC' ? desc : asc
+        state.sortedBy.type = state.sortedBy.type === 'ASC' ? 'DESC' : 'ASC'
+        state.employees.sort(sortType)
+        state.sortedBy.name = action.payload
+      } else {
+        // default sort to ASC
+        state.employees.sort(sortType)
+        state.sortedBy.name = action.payload
+        state.sortedBy.type = 'ASC'
+      }
+      // compute entries
+      computeEntries(state)
+    },
+    setShowEntries: (state, action) => {
+      state.showEntries = parseInt(action.payload)
+      computeEntries(state)
+    },
+    setCurrentPagintation: (state, action) => {
+      state.currentPagination = action.payload
+      // compute entries
+      computeEntries(state)
+    },
+    search: (state, action) => {
+      state.searchValue = action.payload
+      state.currentPagination = 1
+      computeEntries(state)
+    },
   },
 })
 
-export const { add } = createEmployeeSlice.actions
+export const { add, sort, setShowEntries, setCurrentPagintation, search } =
+  employeeSlice.actions
 
-export default createEmployeeSlice.reducer
+export default employeeSlice.reducer
 
 export const selectEmployees = (state) => state.employee.employees
+export const selectSortedBy = (state) => state.employee.sortedBy
+export const selectShowEntries = (state) => state.employee.showEntries
+export const selectCountPaginations = (state) => state.employee.countPaginations
+export const selectCurrentPagination = (state) =>
+  state.employee.currentPagination
+export const selectEntries = (state) => state.employee.entries
+export const selectSearch = (state) => state.employee.searchValue
+export const selectShowing = (state) => state.employee.showing
